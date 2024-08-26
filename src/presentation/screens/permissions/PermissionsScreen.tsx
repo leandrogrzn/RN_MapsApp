cc8647030a1c5989ff809cf8c9fe3a9f80c8683d
Leandro Garzon <hekoej@gmail.com>
2024-08-26 18:00:51 -0500

    PermissionStore - Zustand

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { globalStyles } from "../../../config/theme/styles";
+import { usePermissionStore } from "../../store/permissions/usePermissionsStore";
 
 export const PermissionsScreen = () => {
+
+  const { locationStatus, requestLocationPermission } = usePermissionStore();
+
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{ color: 'black'}}>Habilitar ubicacion</Text>
+
+      <Pressable
+        style={ globalStyles.btnPrimary }
+        onPress={ requestLocationPermission }
+      >
+        <Text style={{ color: 'white' }}>Habilitar localizacion</Text>
+      </Pressable>
+
+      <Text style={{ color: 'black'}}>Estado actual:  { locationStatus }</Text>
+
     </View>
   )
 }
